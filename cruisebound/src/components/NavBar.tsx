869c0df2ad53d078edc61d2b48a2a909c74786f0
omarperezgonzalez <omarperezgonzalez.03@gmail.com>
2025-05-23 14:04:42 -0600

    100% clean navbar component

--- cruisebound/src/components/NavBar.tsx
+++ cruisebound/src/components/NavBar.tsx
@@ -3,7 +3,6 @@
 import { Github } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState, useEffect } from 'react'
 
 export default function Navbar() {
 
